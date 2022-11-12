78f64e0a296544c7fc8e91c1d5f960799afdf5da
Bartosz Panic <bartosz.panic@gmail.com>
2022-11-12 15:49:34 +0100

    change nav menu and add category service on client app

--- eCommerce.NET/Client/Program.cs
+++ eCommerce.NET/Client/Program.cs
@@ -1,7 +1,8 @@
 global using eCommerce.NET.Shared;
 global using System.Net.Http.Json;
+global using eCommerce.NET.Client.Services.CategoryService;
+global using eCommerce.NET.Client.Services.ProductService;
 using eCommerce.NET.Client;
-using eCommerce.NET.Client.Services.ProductService;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -11,5 +12,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
 
 await builder.Build().RunAsync();
