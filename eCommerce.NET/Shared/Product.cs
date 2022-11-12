01384593d94a6b6ebd1bacbecf2952104d60c7ef
Bartosz Panic <bartosz.panic@gmail.com>
2022-11-12 10:14:10 +0100

    first component with static list

---
+++ eCommerce.NET/Shared/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce.NET.Shared
+{
+    public class Product
+    {
+        public virtual int Id { get; set; }
+        public virtual string Title { get; set; } = string.Empty;
+        public virtual string Description { get; set; } = string.Empty;
+        public virtual string ImageUrl { get; set; } = string.Empty;
+        public virtual decimal Price { get; set; } 
+    }
+}
