6269a6de425adc0a1361054de490a7d51401908a
Bartosz Panic <bartosz.panic@gmail.com>
2022-11-16 21:04:29 +0100

    add search bar on client side app

--- eCommerce.NET/Client/Services/ProductService/IProductService.cs
+++ eCommerce.NET/Client/Services/ProductService/IProductService.cs
@@ -5,8 +5,11 @@ namespace eCommerce.NET.Client.Services.ProductService
     public interface IProductService
     {
         event Action ProductsChanged;
+        string Message { get; set; }
         List<Product> Products { get; set; }
         Task GetProducts(string? categoryUrl = null);
         Task<ServiceResponse<Product>> GetProduct(int id);
+        Task SearchProducts(string searchText);
+        Task<List<string>> GetProductsSearchSuggestions(string searchText);
     }
 }
