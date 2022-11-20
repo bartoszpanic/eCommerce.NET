b62c9200e16f28bace9b1d788358f9670aba8489
Bartosz Panic <bartosz.panic@gmail.com>
2022-11-20 22:10:44 +0100

    add simply pagination

--- eCommerce.NET/Server/Services/ProductService/IProductService.cs
+++ eCommerce.NET/Server/Services/ProductService/IProductService.cs
@@ -7,7 +7,7 @@ namespace eCommerce.NET.Server.Services.ProductService
         Task<ServiceResponse<List<Product>>> GetProductsAsync();
         Task<ServiceResponse<Product>> GetProductAsync(int productId);
         Task<ServiceResponse<List<Product>>> GetProductsByCategory(string categoryUrl);
-        Task<ServiceResponse<List<Product>>> SearchProducts(string searchText);
+        Task<ServiceResponse<ProductSearchResult>> SearchProducts(string searchText, int page);
         Task<ServiceResponse<List<string>>> GetProductSearchSuggestions(string searchText);
         Task<ServiceResponse<List<Product>>> GetFeaturedProducts();
     }
