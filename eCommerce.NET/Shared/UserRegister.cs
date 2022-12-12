72e3c5c6c796a924e47dcfd93484c83e85438fe1
Bartosz Panic <bartosz.panic@gmail.com>
2022-12-12 19:40:47 +0100

    Revert "Add register page and dto class"
    This reverts commit ee4bdba8ac381b2f28e41068ef8e22766f8dc04c.

--- eCommerce.NET/Shared/UserRegister.cs
+++
@@ -1,8 +0,0 @@
-namespace eCommerce.NET.Shared;
-
-public class UserRegister
-{
-    public virtual string Email { get; set; } = string.Empty;
-    public virtual string Password { get; set; } = string.Empty;
-    public virtual string ConfirmPassword { get; set; } = string.Empty;
-}
