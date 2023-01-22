1f0c971af6d4deba1ec59630ca8db90d9f86bfb9
Bartosz Panic <bartosz.panic@gmail.com>
2023-01-22 21:16:05 +0100

    add change password to server side and client side

--- eCommerce.NET/Server/Controllers/AuthController.cs
+++ eCommerce.NET/Server/Controllers/AuthController.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using eCommerce.NET.Server.Services.AuthService;
 using eCommerce.NET.Shared;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -37,6 +39,20 @@ namespace eCommerce.NET.Server.Controllers
         public async Task<ActionResult<ServiceResponse<string>>> Login(UserLogin request)
         {
             var response = await _authService.Login(request.Email, request.Password);
+
+            if (!response.Success)
+            {
+                return BadRequest(response);
+            }
+
+            return Ok(response);
+        }
+
+        [HttpPost("change-password"), Authorize]
+        public async Task<ActionResult<ServiceResponse<bool>>> ChangePassword([FromBody] string newPassword)
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var response = await _authService.ChangePassword(int.Parse(userId), newPassword);
 
             if (!response.Success)
             {
